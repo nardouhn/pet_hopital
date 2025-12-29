2eed6f0845111ab02b6cb737635704c553a496ab
Hieu Nguyen <168950622+wokhyu@users.noreply.github.com>
2025-12-29 16:11:58 +0700

    Update SignupPage.jsx

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -1,7 +1,7 @@
 import NavBar from "@/layouts/NavBar";
 import Footer from "@/layouts/Footer";
 import SignupForm from "@/components/auth/SignupForm";
-import { Heart } from "lucide-react";
+import { Heart, PawPrint } from "lucide-react";
 import { Link } from "react-router-dom";
 
 export default function SignupPage() {
@@ -19,16 +19,19 @@ export default function SignupPage() {
         <SignupForm />
 
         <div className="mt-6 w-full max-w-xl bg-white rounded-xl shadow-md p-4">
-          <Link
-            to="/login"
-            className="w-full rounded-xl bg-[linear-gradient(90deg,#14B8A6_0%,#0EA5E9_100%)] py-3 text-white font-semibold flex items-center justify-center gap-2 hover:bg-teal-600 transition"
-          >
-            ♡ Đăng nhập (Log in)
-          </Link>
+          <div className="flex items-center justify-center h-full">
+            <Link
+              to="/login"
+              className="w-1/2 rounded-xl bg-[linear-gradient(90deg,#14B8A6_0%,#0EA5E9_100%)] py-3 text-white font-semibold flex items-center justify-center gap-2 hover:bg-teal-600 transition"
+            >
+              ♡ Đăng nhập (Log in)
+              <PawPrint className="w-5 h-5" />
+            </Link>
+          </div>
         </div>
       </main>
 
-      <Footer />
+      <Footer variant="compact" />
     </div>
   );
 }
